19cdb147ff5d6d5e3a9a08bccb6190115be229cb
Mrcodehunter <muradhossen5267@gmail.com>
2025-10-18 02:03:52 +0600

    initiated structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Navbar';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import About from './Pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <main className="main-with-navbar">
+        {/* your sections with matching IDs */}
+        <section id="home"><Home/></section>
+        <section id="experience"><About/></section>
+        <section id="achievements"><Contact/></section>
+        <section id="projects"><Home/></section>
+        <section id="publications"><About/></section>
+        <section id="posts"><Contact/></section>
+        <section id="contact"><Contact/></section>
+      </main>
+    </>
   );
 }
 
